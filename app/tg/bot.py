ef160f689ba8c71f2f47fdc9ee9cd498618b7dc1
JhonSmith0x7b <JhonSmith0x7b@github.com>
2025-12-03 16:31:40 +0800

    add base url

--- app/tg/bot.py
+++ app/tg/bot.py
@@ -5,22 +5,26 @@ from util import SqlalchemyHelper, BotUser
 import logging
 
 
-
 class TgBot():
-    
+
     def __init__(self):
         self.user_chat_ids = set()
-        self.app = ext.ApplicationBuilder().token(os.environ['TG_BOT_TOKEN']).build()
+        if os.environ.get('TG_API_BASE_URL') is not None:
+            self.app = ext.ApplicationBuilder().token(
+                os.environ['TG_BOT_TOKEN']).base_url(os.environ['TG_API_BASE_URL']).build()
+        else:
+            self.app = ext.ApplicationBuilder().token(
+                os.environ['TG_BOT_TOKEN']).build()
         self.app.add_handler(ext.CommandHandler("update", self.update))
         self.sqlalchemy_helper = SqlalchemyHelper(os.environ['PG_URL'])
         all_users = self.sqlalchemy_helper.get_all_bot_user()
         for user in all_users:
             logging.info(f"Loaded user: {user.name}, chat_id: {user.chat_id}")
             self.user_chat_ids.add(user.chat_id)
-        
+
     async def update(self, update: Update, context: ext.ContextTypes.DEFAULT_TYPE) -> None:
         chat_id = update.effective_chat.id  # type: ignore
-        username = update.effective_user.id # type: ignore
+        username = update.effective_user.id  # type: ignore
         bot_user = BotUser(
             name=username,
             chat_id=chat_id
@@ -31,7 +35,7 @@ class TgBot():
             self.user_chat_ids.add(chat_id)
         else:
             re_message = "Registration failed. You might be already registered."
-        await update.message.reply_text(re_message) # type: ignore
+        await update.message.reply_text(re_message)  # type: ignore
 
     async def send_push_message(self, message: str) -> None:
         for chat_id in self.user_chat_ids:
